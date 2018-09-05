537381e3e66dd537dfd826f709d28b4b2927bc2b
fkerrin <fkerrin@gmail.com>
2018-09-05 23:07:49 +0100

    uploading new code to set up S3 bucket for web

--- 01-WebPublish/WebPublish/web-publish.py
+++ 01-WebPublish/WebPublish/web-publish.py
@@ -3,6 +3,7 @@
 
 import boto3
 import click
+from botocore.exceptions import ClientError  # To catch errors raised through the API
 
 MySession = boto3.Session(profile_name = 'PythonUser')
 
@@ -13,7 +14,7 @@ def CLI():
     'Publish website to S3'  # This docstring is the command description (using the --help option)
     pass
 
-@CLI.command('ListBuckets')
+@CLI.command('ListBuckets')  # This string is the command to use with the script
 def list_buckets():
     "Lists all S3 Buckets in Account"  # This docstring shows in the Command description
     for Bucket in S3.buckets.all():
@@ -26,6 +27,52 @@ def ListBucketObjects(bucket):
     for Object in S3.Bucket(bucket).objects.all():
 	    print(Object)
 
+@CLI.command('SetupBucket')
+@click.argument('bucket')  # Bucket name must be specifieed by a command line argument
+def SetUpBucket(bucket):
+    'Set up S3 bucket to host wehsite'
+# Create the new bucket from the name provided
+    try:
+        NewBucket = S3.create_bucket(Bucket = bucket, CreateBucketConfiguration = {'LocationConstraint' : MySession.region_name})
+# Region needs to be specified explicitly, at least outside us-east-1
+# If the bucket exists there will be an error - if I own it it's OK but if not, the supplied bucket name cannot be used
+    except ClientError as Err:
+	    if Err.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+		    NewBucket = S3.Bucket(bucket)  # Set the bucket as the already existing and owned one
+	    else:
+		    raise Err  # Any other error just raise it and exit program
+
+# Upload the index.html file - note directory separators are / rather than Windows standard \ - I could get click() to take ths as a command line argument
+    NewBucket.upload_file('/AWS/Python/AutomateAWS/AutomatingAWSWithPython/01-WebPublish/WebPublish/index.html', 'index.html', ExtraArgs={'ContentType' : 'text/html'})
+# This copies the specified local file to a file called index.html of text/html format to the specified bucket
+
+# Now we need to create a bucket policy for web hosting permissions (copied from AWS documents on S3 web hosting
+# The """ is for text over multiple lines - the { comes straight after as otherwise BucketPolicy will start with /n and not be accepted
+    BucketPolicy = """{
+      "Version":"2012-10-17",
+      "Statement":[{
+        "Sid":"PublicReadGetObject",
+        "Effect":"Allow",
+        "Principal": "*",
+        "Action":["s3:GetObject"],
+        "Resource":["arn:aws:s3:::%s/*"]
+      }]
+    }""" %NewBucket.name
+# Template string %s gets replaced by what is immediately after the string - i.e. NewBucket.name
+# Now need to apply this to the bucket - done in a roundabout way with boto3 by creating a policy object from the bucket Policy() method and putting that to the bucket
+    NewBucketPolicy = NewBucket.Policy()
+    NewBucketPolicy.put(Policy = BucketPolicy)
+
+# Now need to set web hosting on the bucket - again roundabout - create a Website object and put it on the bucket
+    Website = NewBucket.Website()
+    Website.put(WebsiteConfiguration = {'ErrorDocument' : {'Key' : 'error.html'}, 'IndexDocument' : {'Suffix' : 'index.html'}})
+# 'Suffix' is the default file that is presented if the browser request does not include a specific html page
+
+# Now need to create the URL name from the bucket and region - AWS has a standard format
+    URL = 'http://' + NewBucket.name + '.s3-website-' + MySession.region_name + '.amazonaws.com'
+    print(URL)
+	
+    return
 
 if __name__ == '__main__':
     CLI()  # Delegates control of the functions to teh CLI command group - click will manage the flow
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ I started this course on 3rd September 2018
 
 4th September 2018 - set up AWS session user, set up Python environment with pipenv and created a folder for the first project - Puplishing Websiite to S3
 5th September 2018 - added folder with basic Python script, using @click decorators to give CLI functionality to list S3 buckets and objects
+5th September 2018 - added to script to set up S3 bucket, copy index.html and set up permissions and web hosting
